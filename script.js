9e8cff413aec091bb044b1083fffdb92f2634a3e
Doruk <137659200+dorukgenc@users.noreply.github.com>
2024-03-25 02:47:15 +0300

    update GUI, remove logic.

--- script.js
+++ script.js
@@ -22,43 +22,6 @@ let operatorClicked = false;
 let secondNumberClicked = false;
 let screenText = document.querySelector(".screenText");
 
-function doOperation(element) {
-  if (element === "clear") {
-    screenText.textContent = 0;
-    firstNumber = 0;
-    secondNumber = 0;
-    operator = null;
-    return;
-  }
-
-  if (!firstNumberClicked) {
-    firstNumber = parseFloat(element);
-    firstNumberClicked = true;
-    screenText.textContent = firstNumber;
-  } else if (!operatorClicked) {
-    operator = element;
-    operatorClicked = true;
-    screenText.textContent = firstNumber + " " + operator;
-  } else if (!secondNumberClicked) {
-    secondNumber = parseFloat(element);
-    secondNumberClicked = true;
-    let result =
-      firstNumber +
-      " " +
-      operator +
-      " " +
-      secondNumber +
-      " " +
-      "= " +
-      operate(firstNumber, secondNumber, operator);
-    operate(firstNumber, secondNumber, operator);
-    screenText.textContent = result;
-    firstNumberClicked = false;
-    operatorClicked = false;
-    secondNumberClicked = false;
-  }
-}
-
 function operate(num1, num2, operator) {
   switch (operator) {
     case "+":
@@ -76,7 +39,7 @@ function clickHandler(event) {
   let target = event.target;
   let element = target.textContent;
 
-  doOperation(element);
+  if 
 }
 
 document.addEventListener("click", clickHandler);
