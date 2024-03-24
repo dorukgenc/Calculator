e35dd4148f30abc1d283d7d3846bb6f45ae09859
Doruk <137659200+dorukgenc@users.noreply.github.com>
2024-03-24 23:16:10 +0300

    First version of the GUI, also the calculating logic.

---
+++ script.js
@@ -0,0 +1,33 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+      break;
+    case "-":
+      return subtract(num1, num2);
+      break;
+    case "*":
+      return multiply(num1, num2);
+      break;
+    case "/":
+      return divide(num1, num2);
+      break;
+  }
+}
